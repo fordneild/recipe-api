e765eb11d710963f7b01fae12245f60ca7083988
Ford neild <hanfordn35@gmail.com>
2021-06-01 14:06:30 -0400

    will run code checker

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionManager
+                                        PermissionManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionManager):
+class User(AbstractBaseUser, PermissionManager, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255, null=True)
