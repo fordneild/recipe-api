511b077ed48f38e4cbb9cbc0350173213570fff6
Ford neild <hanfordn35@gmail.com>
2021-06-30 15:06:36 -0400

    added working list tags endpoint

--- app/core/admin.py
+++ app/core/admin.py
@@ -26,3 +26,4 @@ class UserAdmin(BaseUserAdmin):
 
 # Register your models here.
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,12 @@ from django.test import TestCase
 
 from django.contrib.auth import get_user_model
 
+from core import models
+
+def sample_user(email="test@fordneild.com", password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email(self):
@@ -36,4 +42,12 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser('test@londonappdev.com', 'Testpass123')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
 
